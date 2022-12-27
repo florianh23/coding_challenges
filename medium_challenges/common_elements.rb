d8f5ae831f2d648c2c277db3164532e29083acf4
Florian Heinrich <flohein@yahoo.de>
2022-12-27 11:40:56 +0100

    find common elements in arrays

---
+++ medium_challenges/common_elements.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Create a function that takes two lists of numbers sorted in ascending
+# order and returns an array of numbers which are common to both the
+# input arrays.
+
+def common_elements(arr1, arr2)
+  arr1 & arr2
+end
+
+# common_elements([-1, 3, 4, 6, 7, 9], [1, 3])
+
+common_elements([1, 3, 4, 6, 7, 9], [1, 2, 3, 4, 7, 10])
+
+# common_elements([1, 2, 2, 2, 3, 4, 5], [1, 2, 4, 5])
+
+# common_elements([1, 2, 3, 4, 5], [10, 12, 13, 15])
